22e8de491627d83c1a353e1db2d9024f45914b8b
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2022-06-28 12:37:11 +0200

    Update plot_contrast.m
    Merge (C)SPAMM
    Use differen contrast measure
    Set ylim

--- plot_contrast.m
+++ plot_contrast.m
@@ -3,23 +3,22 @@ function plot_contrast(M, M_i, time)
     [~, min_idx] = min(squeeze(M(3, 401, :)));
     [~, max_idx] = max(squeeze(M(3, 401, :)));
 
-    figure('Name', 'Contrast (C)SPAMM')
-    subplot(1, 2, 1)
-    title('SPAMM')
+    figure('Name', 'Contrast (C)SPAMM');
     min_M = squeeze(M(3, :, min_idx));
     max_M = squeeze(M(3, :, max_idx));
-    plot(time, (max_M - min_M) ./ (max_M + min_M), 'linewidth', 2)
-    ylim([0, 20000])
-    ylabel('Contrast')
-    xlabel('Time [s]')
+    plot(time, (max_M - min_M) ./ (max_M), '-.', 'linewidth', 2)
+    ylabel('Contrast', 'FontSize', 24)
+    xlabel('Time [s]', 'FontSize', 24)
 
-    subplot(1, 2, 2)
+    hold on
     CSPAMM = M - M_i;
     min_CSPAMM = squeeze(CSPAMM(3, :, min_idx));
     max_CSPAMM = squeeze(CSPAMM(3, :, max_idx));
-    title('CSPAMM')
-    plot(time, (max_CSPAMM - min_CSPAMM) ./ (max_CSPAMM + min_CSPAMM), 'linewidth', 2)
-    ylim([0, 20000])
-    ylabel('Contrast')
-    xlabel('Time [s]')
+    plot(time, (max_CSPAMM - min_CSPAMM) ./ (max_CSPAMM), '--', 'linewidth', 2)
+
+    legend('SPAMM', 'CSPAMM', 'Location', 'south')
+    ax = gca;
+    ax.FontSize = 16;
+
+    ylim([0, 2.5])
 end
