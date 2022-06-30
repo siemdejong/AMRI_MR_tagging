6f94cb1817f73a81943e0655f83b0fd6e00a28c8
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2022-06-30 20:51:49 +0200

    Change contrast calculation
    Previous contrast calculation was dirty. It is cleaner now. Michelson contrast to make sure CSPAMM contrast makes sense. Rescaling of CSPAMM image, to fairly compare it with SPAMM image.

--- plot_contrast.m
+++ plot_contrast.m
@@ -1,24 +1,36 @@
 function plot_contrast(M, M_i, time)
-    % Search for min/max after last RF pulse
-    [~, min_idx] = min(squeeze(M(3, 401, :)));
-    [~, max_idx] = max(squeeze(M(3, 401, :)));
+
+    Mz = abs(squeeze(M(3, :, :)));
+    Mz_i = abs(squeeze(M_i(3, :, :)));
+    CSPAMMz = Mz - Mz_i;
+
+    min_Mz = zeros(size(time));
+    max_Mz = zeros(size(time));
+    min_CSPAMMz = zeros(size(time));
+    max_CSPAMMz = zeros(size(time));
+
+    disp("Calculating extrema for " + length(time) + " timepoints.")
+    for t = progress(1:length(time))
+        min_Mz(1, t) = min(Mz(t, :));
+        max_Mz(1, t) = max(Mz(t, :));
+
+        % Rescale (minmax normalization), because otherwise unfair
+        % comparison.
+        CSPAMMz(t, :) = rescale(CSPAMMz(t, :));
+        min_CSPAMMz(1, t) = min(CSPAMMz(t, :));
+        max_CSPAMMz(1, t) = max(CSPAMMz(t, :));
+    end
 
     figure('Name', 'Contrast (C)SPAMM');
-    min_M = squeeze(M(3, :, min_idx));
-    max_M = squeeze(M(3, :, max_idx));
-    plot(time, (max_M - min_M) ./ (max_M), '-.', 'linewidth', 2)
+    % Michelson contrast to make sure CSPAMM contrast makes sense.
+    plot(time, (max_Mz - min_Mz)' ./ (max_Mz + min_Mz)', '-.', 'linewidth', 2)
     ylabel('Contrast', 'FontSize', 24)
     xlabel('Time [s]', 'FontSize', 24)
 
     hold on
-    CSPAMM = M - M_i;
-    min_CSPAMM = squeeze(CSPAMM(3, :, min_idx));
-    max_CSPAMM = squeeze(CSPAMM(3, :, max_idx));
-    plot(time, (max_CSPAMM - min_CSPAMM) ./ (max_CSPAMM), '--', 'linewidth', 2)
+    plot(time, (max_CSPAMMz - min_CSPAMMz)' ./ (max_CSPAMMz + min_CSPAMMz)', '--', 'linewidth', 2)
 
     legend('SPAMM', 'CSPAMM', 'Location', 'south')
     ax = gca;
     ax.FontSize = 16;
-
-    ylim([0, 2.5])
 end
